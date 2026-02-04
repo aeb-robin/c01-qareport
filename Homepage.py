2ecbd80a14efac8887ba4b2e8d2c41977138a9f1
RobinWen <24491718+robinwen@users.noreply.github.com>
2026-02-04 20:29:57 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -18,11 +18,11 @@ if platform.system() != "Windows":
 
 load_dotenv()
 
-    DB_HOST = st.secrets["DB_HOST"]
-    DB_PORT = st.secrets["DB_PORT"]
-    DB_USER = st.secrets["DB_USER"]
-    DB_PASSWORD = st.secrets["DB_PASSWORD"]
-    DB_NAME = st.secrets["DB_NAME"]
+DB_HOST = st.secrets["DB_HOST"]
+DB_PORT = st.secrets["DB_PORT"]
+DB_USER = st.secrets["DB_USER"]
+DB_PASSWORD = st.secrets["DB_PASSWORD"]
+DB_NAME = st.secrets["DB_NAME"]
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}
 
 
