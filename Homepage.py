c2ccc7b68cba178300c4ff8f132baef80de64b61
RobinWen <24491718+robinwen@users.noreply.github.com>
2026-02-04 20:30:44 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -23,7 +23,7 @@ DB_PORT = st.secrets["DB_PORT"]
 DB_USER = st.secrets["DB_USER"]
 DB_PASSWORD = st.secrets["DB_PASSWORD"]
 DB_NAME = st.secrets["DB_NAME"]
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 
