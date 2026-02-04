fc7525b8bd34e07b42beba8476ba5cae2d78032e
RobinWen <24491718+robinwen@users.noreply.github.com>
2026-02-04 19:57:13 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -19,10 +19,11 @@ if platform.system() != "Windows":
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # 建立Logger
 logger = logging.getLogger('my_logger')
